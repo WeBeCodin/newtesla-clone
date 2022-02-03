582a03b9404fa9099ee08d39906ffd50def5e1a0
WeBeCodin <93334652+WeBeCodin@users.noreply.github.com>
2022-02-03 22:42:56 +0800

    Finished Tesla Clone

---
+++ src/components/Home.js
@@ -0,0 +1,66 @@
+import React from "react";
+import Section from "./Section";
+import styled from "styled-components";
+import uniqid from "uniqid";
+
+function Home() {
+  return (
+    <Container>
+      <Section
+        title="Model S"
+        description="Order Online For Touchles Delivery"
+        backgroundImg="model-s.jpg"
+        leftBtnText="Custom order"
+        rightBtnText="Existing inventory"
+      />
+      <Section
+        title="Model Y"
+        description="Order Online For Touchles Delivery"
+        backgroundImg="model-y.jpg"
+        leftBtnText="Custom order"
+        rightBtnText="Existing inventory"
+      />
+      <Section
+        title="Model 3"
+        description="Order Online For Touchles Delivery"
+        backgroundImg="model-3.jpg"
+        leftBtnText="Custom order"
+        rightBtnText="Existing inventory"
+      />
+      <Section
+        title="Model X"
+        description="Order Online For Touchles Delivery"
+        backgroundImg="model-x.jpg"
+        leftBtnText="Custom order"
+        rightBtnText="Existing inventory"
+      />
+      <Section
+        title="Lowest Cost Solar Panels In America"
+        description="Money-back guarantee"
+        backgroundImg="solar-panel.jpg"
+        leftBtnText="Order Now"
+        rightBtnText="Learn More"
+      />
+      <Section
+        // id={uniqid(slr)}
+        title="Solar For New Roofs"
+        description="Solar Roof Costs Less Than A New Roof Plus Solar Panels"
+        backgroundImg="solar-roof.jpg"
+        leftBtnText="Order Now"
+        rightBtnText="Learn More"
+      />
+      <Section
+        title="Accessories"
+        description=""
+        backgroundImg="accessories.jpg"
+        leftBtnText="Shop Now"
+      />
+    </Container>
+  );
+}
+
+export default Home;
+
+const Container = styled.div`
+  height: 100vh;
+`;
